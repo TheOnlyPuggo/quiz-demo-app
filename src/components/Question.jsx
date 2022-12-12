5905b96cbd44b271f9ae7b1d5302c99429e3e8de
Pug <iakingdoms@gmail.com>
2022-12-12 20:06:33 +1100

    Made the buttons components

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import QuestionButton from "./QuestionButton";
+
 function Question() {
 	const [buttonStates, setButtonStates] = useState([
 		false,
@@ -8,10 +10,10 @@ function Question() {
 		false,
 	]);
 
-	const updateButtonState = (event) => {
+	const updateButtonState = (id) => {
 		let newArray = [];
 		buttonStates.map((state, index) => {
-			if (event.target.name != index) {
+			if (id != index) {
 				newArray.push(false);
 			} else {
 				newArray.push(true);
@@ -21,91 +23,22 @@ function Question() {
 		setButtonStates(newArray);
 	};
 
-	useEffect(() => {
-		console.log(buttonStates);
-	}, [buttonStates]);
-
 	return (
-		<div className="relative border-b border-b-gray-700 w-[700px] h-32 mt-8">
+		<div className="relative border-b border-b-gray-700 w-[800px] h-32 mt-8">
 			<p className="text-white text-center text-2xl">
 				Why did the titanic sink?
 			</p>
-			<div className="mt-[50px] flex justify-between">
-				{!buttonStates[0] && (
-					<button
-						name="0"
-						className="text-white border border-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
-						onClick={updateButtonState}
-					>
-						Answer 1
-					</button>
-				)}
-				{buttonStates[0] && (
-					<button
-						name="0"
-						className="text-white bg-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
-						onClick={updateButtonState}
-					>
-						Answer 1
-					</button>
-				)}
-
-				{!buttonStates[1] && (
-					<button
-						name="1"
-						className="text-white border border-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
-						onClick={updateButtonState}
-					>
-						Answer 2
-					</button>
-				)}
-				{buttonStates[1] && (
-					<button
-						name="1"
-						className="text-white bg-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
-						onClick={updateButtonState}
-					>
-						Answer 2
-					</button>
-				)}
-
-				{!buttonStates[2] && (
-					<button
-						name="2"
-						className="text-white border border-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
-						onClick={updateButtonState}
-					>
-						Answer 3
-					</button>
-				)}
-				{buttonStates[2] && (
-					<button
-						name="2"
-						className="text-white bg-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
-						onClick={updateButtonState}
-					>
-						Answer 3
-					</button>
-				)}
-
-				{!buttonStates[3] && (
-					<button
-						name="3"
-						className="text-white border border-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
-						onClick={updateButtonState}
-					>
-						Answer 4
-					</button>
-				)}
-				{buttonStates[3] && (
-					<button
-						name="3"
-						className="text-white bg-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
-						onClick={updateButtonState}
-					>
-						Answer 4
-					</button>
-				)}
+			<div className="mt-[50px] flex justify-center">
+				{buttonStates.map((state, index) => {
+					return (
+						<QuestionButton
+							index={index}
+							buttonStates={buttonStates}
+							answer={`Answer ${index + 1}`}
+							updateButtonState={updateButtonState}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
---
+++ src/components/QuestionButton.jsx
@@ -0,0 +1,24 @@
+function QuestionButton(props) {
+	return (
+		<div>
+			{!props.buttonStates[props.index] && (
+				<button
+					onClick={() => props.updateButtonState(props.index)}
+					className="text-white border border-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
+				>
+					{props.answer}
+				</button>
+			)}
+			{props.buttonStates[props.index] && (
+				<button
+					onClick={() => props.updateButtonState(props.index)}
+					className="text-white bg-cyan-600 rounded-lg w-44 h-10 mx-5 hover:bg-cyan-700 active:bg-cyan-900"
+				>
+					{props.answer}
+				</button>
+			)}
+		</div>
+	);
+}
+
+export default QuestionButton;
